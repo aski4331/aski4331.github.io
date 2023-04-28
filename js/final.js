c1cab5bb002fd6c30f41b745583b6e78158d744a
Aspen Kimlicko <aski4331@colorado.edu>
2023-04-28 10:33:41 -0600

    final working midway

--- js/final.js
+++ js/final.js
@@ -18,7 +18,11 @@ threeButton.addEventListener('click', greenClick);
 const fourButton = document.querySelector('#four');
 fourButton.addEventListener('click', blueClick);
 
-const startButton = document.querySelector('#start').addEventListener('click', start);
+const startButton = document.querySelector('#start');
+startButton.addEventListener('click', start); //start
+
+const sequenceButton = document.querySelector('#sequence');
+sequenceButton.addEventListener('click', getSequence);
 
 /*
 function oneFun()
@@ -45,11 +49,14 @@ function fourFun()
 
 
 // function to generate random number from ica12
-function random(min, max) {
+function random(min, max) 
+{
     const num = Math.floor(Math.random() * (max - min + 1)) + min;
     return num;
-  }
+}
+//end random function
 
+//arrays:
 
 let compSequence = [];
 let userSequence = [];
@@ -57,22 +64,24 @@ let userSequence = [];
 let sequenceNum = 0;
 let userNum = 0;
 
+function start()
+{
+    startButton.style.visibility = "hidden";
+    sequenceButton.style.visibility = "visible";
+    userNum = 0;
+    userSequence = [];
+    timeStart();
+}
 
-    //when button is clicked, check if it is black, if it is, (new function) check if it was clicked soon enough
-    // pick a new button to turn black
-    //if not, failure
-
-    //store array of random sequence
-    //when user clicks, store 
 
 //source for start and end functions for timing:
 //https://stackoverflow.com/questions/41632942/how-to-measure-time-elapsed-on-javascript
-
 var startTime, endTime;
 
-function start() {
+function timeStart() 
+{
   startTime = new Date();
-};
+}
 
 function end() {
   endTime = new Date();
@@ -83,34 +92,71 @@ function end() {
   // get seconds 
   var seconds = Math.round(timeDiff);
   console.log(seconds + " seconds");
-  return seconds;
+  return seconds; //returns the number of seconds passed
 }
+//end timing function 
 
-let numClicks = 0;
 
-function redClick(i)
+
+function redClick()
 {
+    userSequence[userNum] = 1; //add to array
+    userNum++; //increment number of items in array
+    //numClicks++; //increment number of clicks
 
-    userSequence[i] = 1; //add to array
-    userNum++;
-
-    if (end() > 2000) //check time
-    {
-        return false;
-    }
-
-    if (userSequence[i] == compSequence[i]) //checks if click was correct
-    {
-        start();
-        return true;
-    }
-
+    if (end() > 2) //check time, over two seconds
+        { console.log("too slow"); failed(); } //run failure function if time is over
+    else if (userSequence[userNum - 1] === compSequence[userNum - 1]) //checks if click was correct
+        { timeStart(); }  //starts new time
     else
-    {
-        return false;
-    }
-
+        { console.log(userSequence[userNum - 1] + " =? " + compSequence[userNum - 1]); failed(); } //run failure function
 }
+
+function yellowClick()
+{
+    userSequence[userNum] = 2; //add to array
+    userNum++; //increment number of items in array
+    //numClicks++; //increment number of clicks
+
+    if (end() > 2) //check time, over two seconds
+        { failed(); } //run failure function if time is over
+    else if (userSequence[userNum - 1] === compSequence[userNum - 1]) //checks if click was correct
+        { timeStart(); }  //starts new time
+    else
+        { console.log(userSequence[userNum - 1] + " =? " + compSequence[userNum - 1]); failed(); } //run failure function
+}
+
+function greenClick()
+{
+    userSequence[userNum] = 3; //add to array
+    userNum++; //increment number of items in array
+    //numClicks++; //increment number of clicks
+
+    if (end() > 2) //check time, over two seconds
+        { failed(); } //run failure function if time is over
+    else if (userSequence[userNum - 1] === compSequence[userNum - 1]) //checks if click was correct
+        { timeStart(); }  //starts new time
+    else
+        { console.log(userSequence[userNum - 1] + " =? " + compSequence[userNum - 1]); failed(); } //run failure function
+}
+
+function blueClick()
+{
+    userSequence[userNum] = 4; //add to array
+    userNum++; //increment number of items in array
+    //numClicks++; //increment number of clicks
+
+    if (end() > 2) //check time, over two seconds
+        { failed(); } //run failure function if time is over
+    else if (userSequence[userNum - 1] === compSequence[userNum - 1]) //checks if click was correct
+        { timeStart(); }  //starts new time
+    else
+        { console.log(userSequence[userNum - 1] + " =? " + compSequence[userNum - 1]); failed(); } //run failure function
+}
+
+
+
+//style.visibility="hidden" "visible"
 
 
 //Source for delay function:
@@ -119,53 +165,96 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 
 async function getSequence()
 {
-    sequence = [random(1, 4)];
-    sequenceNum++;
+    sequenceButton.style.visibility = "hidden";
 
-    for(let i = 0; i < sequenceNum; i++)
+    compSequence[sequenceNum] = random(1, 4); //increase sequence
+    console.log(compSequence[sequenceNum]);
+    sequenceNum++; //increment number of items in sequence
+
+    for(let i = 0; i < sequenceNum; i++) //for each item in sequence
     {
         //flash buttons
-        if (sequence[i] == 1)
+        if (compSequence[i] === 1)
         {
             //red
+            console.log("red");
             oneButton.style.backgroundColor = "black";
             twoButton.style.backgroundColor = "yellow";
             threeButton.style.backgroundColor = "green";
             fourButton.style.backgroundColor = "blue";
+            //await delay(500)
         }
 
-        if (sequence[i] == 2)
+        if (compSequence[i] === 2)
         {
             //yellow
+            console.log("yellow");
             oneButton.style.backgroundColor = "red";
             twoButton.style.backgroundColor = "black";
             threeButton.style.backgroundColor = "green";
             fourButton.style.backgroundColor = "blue";
         }
 
-        if (sequence[i] == 3)
+        if (compSequence[i] === 3)
         {
             //green
+            console.log("green");
             oneButton.style.backgroundColor = "red";
             twoButton.style.backgroundColor = "yellow";
             threeButton.style.backgroundColor = "black";
             fourButton.style.backgroundColor = "blue";
         }
 
-        if (sequence[i] == 4)
+        if (compSequence[i] === 4)
         {
             //blue
+            console.log("blue");
             oneButton.style.backgroundColor = "red";
             twoButton.style.backgroundColor = "yellow";
             threeButton.style.backgroundColor = "green";
             fourButton.style.backgroundColor = "black";
         }
 
-        await delay(1000);
+        await delay(500);
 
     }
 
-    //unhide the start button
+    oneButton.style.backgroundColor = "red";
+    twoButton.style.backgroundColor = "yellow";
+    threeButton.style.backgroundColor = "green";
+    fourButton.style.backgroundColor = "blue";
+
+    startButton.style.visibility = "visible";
+
+
+
+}
+
+
+function checkSequence()
+{
+    startButton.style.visibility = "hidden";
+    sequenceButton.style.visibility = "visible";
+
+    for (j = 0; j < sequenceNum; j++)
+    {
+       console.log(compSequence[j] + ", "); 
+    }
+}
+
+
+
+function failed()
+{
+    //clear all arrays
+    //let clearArray = [];
+    compSequence = [];
+    userSequence = [];
+    sequenceNum = 0;
+    userNum = 0;
+    console.log("Game Over!");
+
+    //display a failure message
 
 }
 
@@ -174,6 +263,13 @@ async function getSequence()
 
 
 
+
+    //when button is clicked, check if it is black, if it is, (new function) check if it was clicked soon enough
+    // pick a new button to turn black
+    //if not, failure
+
+    //store array of random sequence
+    //when user clicks, store 
 
 /*
 function startFun()
